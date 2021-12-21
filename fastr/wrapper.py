b1f8c9666d9b325eaa006fbbb1fdf88ed3c3febc
João Rafael <joaorafaelx@gmail.com>
2021-12-20 23:57:18 -0300

    Remove debug prints

--- fastr/wrapper.py
+++ fastr/wrapper.py
@@ -4,7 +4,7 @@ from fastr_adequate import fastPlusPlus
 from test_parser import coverage_parser, blackbox_parser
 #from example_maker import create_example_bbox_file, create_example_lines_file
 
-print("FASTR WRAPPER")
+# print("FASTR WRAPPER")
 
 # Argument check
 if (len(sys.argv) != 5):
@@ -15,10 +15,10 @@ tests_folder = sys.argv[1]
 coverage_file = "/fastr/coverage.xml"
 lines_file = sys.argv[3]
 bbox_file = sys.argv[4]
-print("Tests folder: " + tests_folder)
-print("Coverage file: " + coverage_file)
-print("Lines file: " + lines_file)
-print("BBOX file: " + bbox_file)
+# print("Tests folder: " + tests_folder)
+# print("Coverage file: " + coverage_file)
+# print("Lines file: " + lines_file)
+# print("BBOX file: " + bbox_file)
 
 blackbox_parser(tests_folder, bbox_file)
 coverage_parser(coverage_file, lines_file)
@@ -29,20 +29,20 @@ coverage_parser(coverage_file, lines_file)
 
 
 # Execute Test Reduction
-print("\n\nFAST++")
+# print("\n\nFAST++")
 fastr_output = fastPlusPlus(bbox_file, lines_file)
-print("Output: " + str(fastr_output))
+# print("Output: " + str(fastr_output))
 # Get only the tests that should be executed
 reduced_tests = fastr_output[3]
-print("Reduced Tests: " + str(reduced_tests))
+# print("Reduced Tests: " + str(reduced_tests))
 
 # Test index starts from 1, subtract 1 from each of them
 reduced_tests = [int(x) - 1 for x in reduced_tests]
 
-print("\n\nRESULTS\n")
+print("FAST-R Reduced Tests\n")
 # Match the test index with the test names in BBOX
 with open(bbox_file) as f:
     for pos, i in enumerate(f):
         if pos in reduced_tests:
             test_path = i.split()[0]
-            print(str(pos + 1) + " --- " + str(test_path))
+            print(str(test_path))
