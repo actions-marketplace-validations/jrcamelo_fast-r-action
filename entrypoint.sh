8017556f42359ac3a04813236c5e4c7998de4395
João Rafael <joaorafaelx@gmail.com>
2021-12-20 21:18:47 -0300

    test entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,18 @@ echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 sh -c "$*"
 
+echo 'running1?'
+echo '::set-output name=action_echo::enabled'
+python3 -m fastr_wrapper test1 test2 linestest bboxtest > result.txt
+
+echo '::set-output name=action_echo::enabled'
+echo 'running3?'
+echo '::set-output name=result::$(cat result.txt)'
+echo '::set-output name=result::aaaaaaaaaaaaaaaa'
+
+echo '::set-output name=action_echo::enabled'
+echo 'running4?'
+
+
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
