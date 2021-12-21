869c2e8d3e13b1ba752ae85bae3ce7f9d0468d7f
João Rafael <joaorafaelx@gmail.com>
2021-12-20 22:42:02 -0300

    Try /

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,10 @@ echo "#################################################"
 echo "#################################################"
 #ls -R "~/"
 echo "#################################################"
-echo "${RUNNER_WORKSPACE}"
-#ls -R "${RUNNER_WORKSPACE}"
+echo "/"
+ls "/"
+echo "#################################################"
+ls "~/"
 echo "#################################################"
 
 echo '::set-output name=action_echo::enabled'
@@ -23,7 +25,7 @@ echo '::set-output name=result::aaaaaaaaaaaaaaaa'
 
 echo 'running1?'
 echo '::set-output name=action_echo::enabled'
-python3 "${RUNNER_WORKSPACE}/fastr/fastr_wrapper.py" "${GITHUB_WORKSPACE}/${INPUT_TEST_FOLDER}" "${GITHUB_WORKSPACE}/${INPUT_TEST_COVERAGE}" "${GITHUB_WORKSPACE}/${INPUT_LINES_FILE}" "${GITHUB_WORKSPACE}/${INPUT_BBOX_FILE}" > result.txt
+python3 "/fastr/fastr_wrapper.py" "${GITHUB_WORKSPACE}/${INPUT_TEST_FOLDER}" "${GITHUB_WORKSPACE}/${INPUT_TEST_COVERAGE}" "${GITHUB_WORKSPACE}/${INPUT_LINES_FILE}" "${GITHUB_WORKSPACE}/${INPUT_BBOX_FILE}" > result.txt
 echo '::set-output name=result::$(cat result.txt)'
 
 
